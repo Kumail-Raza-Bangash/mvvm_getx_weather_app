5d283f18da649c80158e429b0d32a494cd77985d
Kumail-Raza-Bangash <kumailraza260@gmail.com>
2024-04-19 15:56:38 +0500

    Updated Code

--- lib/resourses/routes/routes.dart
+++ lib/resourses/routes/routes.dart
@@ -7,7 +7,7 @@ class AppRoutes {
   static appRoute() => [
         GetPage(
           name: RoutesName.initialRoute,
-          page: () => const LocationView(), //HomeView
+          page: () => const HomeView(), //HomeView LocationView
           transition: Transition.fadeIn,
           transitionDuration: const Duration(milliseconds: 250),
         ),
