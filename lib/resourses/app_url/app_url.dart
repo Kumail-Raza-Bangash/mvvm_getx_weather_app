ddee5c0cacbb47997f81dc8a99ad37cb9549ec63
Kumail-Raza-Bangash <kumailraza260@gmail.com>
2024-04-22 11:41:39 +0500

    Updated Code MODELVIEW

--- lib/resourses/app_url/app_url.dart
+++ lib/resourses/app_url/app_url.dart
@@ -1,4 +1,5 @@
 class URLs {
-  static String baseUrl = 'https://api.openweathermap.org/data/2.5/weather?q=Lahore&appid=3d30c57a037dc01fb7e2feda92e83723';
-  static String getWeatherData = '';
+  static String baseUrl = 'https://api.openweathermap.org/data/2.5/weather?q=Lahore&appid=$urlKey';
+  static String urlKey = '3d30c57a037dc01fb7e2feda92e83723';
+  String getWeatherData = '';
 }
--- lib/view_model/home/home_view_model.dart
+++ lib/view_model/home/home_view_model.dart
@@ -1,5 +1,55 @@
+import 'dart:convert';
+
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:mvvm_getx_weather_app/model/weather_model.dart';
+import 'package:mvvm_getx_weather_app/resourses/helper/getx_helper.dart';
+import 'package:mvvm_getx_weather_app/resourses/helper/global_variables.dart';
+import 'package:http/http.dart' as http;
 
 class HomeViewModel extends GetxController {
-  
+  Rx<WeatherModel> weatherModel = WeatherModel().obs;
+  RxString location = "Islamabad, Pakistan".obs;
+
+  void onReady(){
+    super.onReady();
+  }
+
+  getWeatherUpdate () async {
+    try {
+      Map<String, String> header = {
+        'Counter-Type' : 'application/json',
+        'Accept' : 'application/json'
+      };
+
+      String url = 'https://api.openweathermap.org/data/2.5/weather';
+      Map<String, String> params = {
+        'appid': '3d30c57a037dc01fb7e2feda92e83723',
+        'q': location.value,
+        'units': 'metric',
+      };
+      Uri uriValue = Uri.parse(url).replace(queryParameters: params);
+
+      GlobalVariables.showLoader.value;
+      http.Response response = await http.get(uriValue,headers: header);
+      Map<String, dynamic> parsedJson = jsonDecode(response.body);
+      GlobalVariables.showLoader.value = false;
+
+      if(parsedJson['cod'] == 200){
+        weatherModel.value = WeatherModel.fromJson(parsedJson);
+
+        //Store Data Locally
+        GetStorage().write(location.value, parsedJson);
+        GetxHelper.showSnackbar(message: 'Weather Updated Successfully');
+      }
+      else{
+        GetxHelper.showSnackbar(message: 'Something went wrong');
+      }
+
+    }
+    catch (e){
+
+    }
+  }
+
 }
